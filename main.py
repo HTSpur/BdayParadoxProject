cd1a13590d1324e17b68307c3535a02ae65ad911
HT_Spur <0459420@gmail.com>
2024-05-21 11:52:09 -0400

    added dupe checker function!!!

--- main.py
+++ main.py
@@ -19,3 +19,35 @@ months_dict = {
 }
 
 birthday_list = []
+
+def generate_birthdays(num):
+    birthdays = []
+    for i in range(num):
+        month = random.choice(list(months_dict.keys()))
+        day = random.randint(1, months_dict[month])
+        birthdays.append(f"{month} {day}")
+
+    return birthdays
+
+
+#x = generate_birthdays(int(bnum))
+#print(x)
+
+
+#generate list 100,000 times
+#check for dupes
+
+def dupe_tester():
+    global dupes
+    dupes = 0
+    for i in range(100000):
+        x = generate_birthdays(int(bnum))
+        if len(x) == len(set(x)):
+            continue
+        else:
+            x = generate_birthdays(int(bnum))
+            dupes += 1
+
+
+dupe_tester()
+print(dupes)
